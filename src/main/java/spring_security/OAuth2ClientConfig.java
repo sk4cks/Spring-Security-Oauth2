f2d01d51fc21cc4e25042f34de35c04c045eb409
승찬 나 <sk4cks@naver.com>
2025-01-15 20:52:57 +0900

    OAuth2AuthorizationRequestResolver

--- src/main/java/spring_security/OAuth2ClientConfig.java
+++ src/main/java/spring_security/OAuth2ClientConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -22,31 +23,19 @@ public class OAuth2ClientConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request -> request
-                .requestMatchers("login").permitAll()
+                .requestMatchers("home").permitAll()
                 .anyRequest().authenticated());
-
-        http.oauth2Login(oauth2 -> oauth2.loginPage("/login")
-//                .loginProcessingUrl("/login/v1/oauth2/code/*")
-                .authorizationEndpoint(authorizationEndpointConfig ->
-                        authorizationEndpointConfig.baseUri("/oauth2/v1/authorization"))
-                .redirectionEndpoint(redirectionEndpointConfig ->
-                        redirectionEndpointConfig.baseUri("/login/v1/oauth2/code/*"))
-                .defaultSuccessUrl("/loginPage"));
-
+        http.oauth2Login(authLogin ->
+                authLogin.authorizationEndpoint(authEndPoint ->
+                        authEndPoint.authorizationRequestResolver(customOAuth2AuthenticationRequestResolver())));
         http.logout(logout -> logout
-                .logoutUrl("/logout")
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
-                .logoutSuccessHandler(oidcLogoutSuccessHandler())
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID"));
+                .logoutSuccessUrl("/home"));
         return http.build();
     }
 
-    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
-        OidcClientInitiatedLogoutSuccessHandler  successHandler = new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
-        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
-
-        return successHandler;
+    private OAuth2AuthorizationRequestResolver customOAuth2AuthenticationRequestResolver() {
+        return new CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
     }
+
+
 }
