0f0f48079559a7fd4a3330332ca51ab4f469f1bf
승찬 나 <sk4cks@naver.com>
2025-02-11 20:11:31 +0900

    DefaultOAuth2AuthorizedClientManager - Resource Owner Password 권한 부여 구현하기

--- src/main/java/spring_security/config/AppConfig.java
+++ src/main/java/spring_security/config/AppConfig.java
@@ -1,19 +1,24 @@
 package spring_security.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public OAuth2AuthorizedClientManager auth2AuthorizedClientManager(ClientRegistrationRepository clientRegistrationRepository,
+    public DefaultOAuth2AuthorizedClientManager auth2AuthorizedClientManager(ClientRegistrationRepository clientRegistrationRepository,
                                                                       OAuth2AuthorizedClientRepository clientRepository) {
         OAuth2AuthorizedClientProvider oAuth2AuthorizedClientProvider =
                 OAuth2AuthorizedClientProviderBuilder.builder()
@@ -27,7 +32,23 @@ public class AppConfig {
                 new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,clientRepository);
 
         oAuth2AuthorizedClientManager.setAuthorizedClientProvider(oAuth2AuthorizedClientProvider);
+        oAuth2AuthorizedClientManager.setContextAttributesMapper(contextAttributesMapper());
 
         return oAuth2AuthorizedClientManager;
+    }
+
+    private Function<OAuth2AuthorizeRequest, Map<String, Object>> contextAttributesMapper() {
+        return oAuth2AuthorizeRequest -> {
+            Map<String,Object> contextAttributes = new HashMap<>();
+            HttpServletRequest request = oAuth2AuthorizeRequest.getAttribute(HttpServletRequest.class.getName());
+            String username = request.getParameter(OAuth2ParameterNames.USERNAME);
+            String password = request.getParameter(OAuth2ParameterNames.PASSWORD);
+
+            if(StringUtils.hasText(username) && StringUtils.hasText(password)) {
+                contextAttributes.put(OAuth2AuthorizationContext.USERNAME_ATTRIBUTE_NAME, username);
+                contextAttributes.put(OAuth2AuthorizationContext.PASSWORD_ATTRIBUTE_NAME, password);
+            }
+            return contextAttributes;
+        };
     }
 }
--- src/main/java/spring_security/controller/LoginController.java
+++ src/main/java/spring_security/controller/LoginController.java
@@ -2,7 +2,24 @@ package spring_security.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationContext;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,12 +28,63 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 @Controller
 public class LoginController {
 
+    @Autowired
+    private DefaultOAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
+
+    @Autowired
+    private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
+
     @GetMapping("/oauth2Login")
     public String oauth2Login(Model model, HttpServletRequest request, HttpServletResponse response) {
-        return "redirect:/";
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
+                .withClientRegistrationId("keycloak1")
+                .principal(authentication)
+                .attribute(HttpServletRequest.class.getName(), request)
+                .attribute(HttpServletResponse.class.getName(), response)
+                .build();
+
+        OAuth2AuthorizationSuccessHandler successHandler = (authorizedClient, principal, attributes) -> {
+            this.oAuth2AuthorizedClientRepository
+                    .saveAuthorizedClient(authorizedClient, principal,
+                            (HttpServletRequest) attributes.get(HttpServletRequest.class.getName()),
+                            (HttpServletResponse) attributes.get(HttpServletResponse.class.getName()));
+            System.out.println("authorizedClient = " + authorizedClient);
+            System.out.println("principal = " + principal);
+            System.out.println("attributes = " + attributes);
+        };
+
+        this.oAuth2AuthorizedClientManager.setAuthorizationSuccessHandler(successHandler);
+
+        OAuth2AuthorizedClient authorizedClient = this.oAuth2AuthorizedClientManager.authorize(authorizeRequest);
+
+        if(authorizedClient != null) {
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
+            ClientRegistration clientRegistration = authorizedClient.getClientRegistration();
+            OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+            OAuth2UserRequest oAuth2UserRequest = new OAuth2UserRequest(clientRegistration,accessToken);
+            OAuth2User oAuth2User = oAuth2UserService.loadUser(oAuth2UserRequest);
+
+            SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
+            authorityMapper.setPrefix("SYSTEM_");
+            Set<GrantedAuthority> grantedAuthorities = authorityMapper.mapAuthorities(oAuth2User.getAuthorities());
+
+            OAuth2AuthenticationToken oAuth2AuthenticationToken =
+                    new OAuth2AuthenticationToken(oAuth2User, grantedAuthorities, clientRegistration.getRegistrationId());
+
+            SecurityContextHolder.getContext().setAuthentication(oAuth2AuthenticationToken);
+
+            model.addAttribute("oAuth2AuthenticationToken", oAuth2AuthenticationToken);
+        }
+
+        return "home";
     }
 
     @GetMapping("/logout")
